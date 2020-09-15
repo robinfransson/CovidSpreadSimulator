af7565baaa56e4377ef063a05416369c27126cd1
robinfransson <71066647+robinfransson@users.noreply.github.com>
2020-09-16 00:17:49 +0200

    Fixade till TryInfect()
    egentligen är det här min första uppladdning, första var bara för att föra över till en annan dator

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CovidSpreadSimulator
@@ -12,17 +14,15 @@ namespace CovidSpreadSimulator
         static void Main()
         {
             CreatePersons();
-            while (true)
-            {
-                TryInfect();
-            }
+            TryInfect();
+
         }
         static void CreatePersons()
         {
             for (int i = 0; i < 50; i++)
             {
                 bool infected;
-                if (i == 0)
+                if (i == 0) // första ska vara smittsam
                 {
                     infected = true;
                 }
@@ -30,52 +30,92 @@ namespace CovidSpreadSimulator
                 {
                     infected = false;
                 }
-                Person p = new Person(infected, 0, false);
+                Person p = new Person(infected, false, i);
                 persons.Add(p);
             }
         }
         static void TryInfect()
         {
-            var isInfected = from person in persons
-                             where person.infected == true
-                             && person.infectedHours < 5
-                             select person;
-            var isImmune = from person in persons
-                           where person.immune == true
-                           select person;
-            var canSpreadDisease = from person in persons
-                                   where person.infected == true
-                                   && person.infectedHours < 5
-                                   select person;
-            Console.WriteLine(persons.Select(isInfected)); ;
-            //foreach(Person p in persons)
-            //{
-            //    if(isInfected.Count() > 0)
-            //    { 
-            //    
-            //    }
-            //}
+            int numberOfImmune = 0;
+            int toInfect = 0;
+            int hoursPassed = 0;
+            while (numberOfImmune < 50)
+            {
+                var Infected = persons.Where(x => x.isInfected);
+                int numberOfInfected = Infected.Count(); // hur många som är infekterade
+                var selectImmune = persons.Where(x => x.immune);
+                numberOfImmune = selectImmune.Count(); // hur många som är immuna
+                var notInfected = persons.Where(x => !x.isInfected);
+                int numberOfNotInfected = notInfected.Count();// hur många som inte är infekterade
+                toInfect = numberOfInfected; // här sätts variabeln för hur många som ska infekteras nästa loop
+                foreach (var person in persons)
+                {
+                    //Console.WriteLine("{0}, {1}",person.iteration, person.infectedHours);
+                    Thread.Sleep(50);
+                    if (toInfect > numberOfNotInfected) // om det är fler att infektera än vad det är icke infekterade sätts variablen till antalet icke infekterade
+                    {
+                        toInfect = numberOfNotInfected;
+                    }
+                    if (!person.immune) // om personen inte är immun
+                    {
+                        if (toInfect == 0 && numberOfNotInfected > 0) // om det är den sista som ska infekteras behövs inte loopen köras längre
+                        {
+                            person.isInfected = true;
+                            break;
+                        }
+                        else if (person.CanSpreadDisease())
+                        {
+                            if (person.infectedHours > 3) // efter 4 timmar blir personen immun
+                            {
+                                person.immune = true;
+                            }
+                            else
+                            {
+                                person.infectedHours++;
+                            }
+                        }
+                        else if (!person.CanSpreadDisease() && toInfect >= 1) // om personen inte kan sprida sjukdom ska personen bli infekterad om det finns kvar 
+                        {                                                     //personer som kan infektera denna loop 
+                            person.isInfected = true;
+                            toInfect--;
+                        }
+
+                    }
+
+                }
+                hoursPassed++;
+                Console.Clear();
+                Console.WriteLine($"{hoursPassed} hours has passed, there are {numberOfInfected-numberOfImmune} infected among us, {numberOfImmune}" +
+                    $" people are immune.\nThere are {numberOfNotInfected} people not infected");
+                Thread.Sleep(50);
+            }
+            Console.ReadLine();
         }
     }
     class Person
     {
-        public bool infected;
+        public int iteration;
+        public bool isInfected;
         public int infectedHours;
         public bool immune;
-        public Person(bool infected, int infectedHours, bool immune)
+        public Person(bool infected, bool immune, int id)
         {
             this.immune = immune;
-            this.infected = infected;
-            this.infectedHours = infectedHours;
+            this.isInfected = infected;
+            this.infectedHours = 0;
+            this.iteration = id;
         }
 
-        public bool canSpreadDisease()
+        public bool CanSpreadDisease() // om personen är infekterad och inte immun kan den sprida sjukdom
         {
-            if (this.infected == true && this.infectedHours < 5)
+            if (this.isInfected == true && !this.immune)
             {
                 return true;
             }
-            return false;
+            else
+            {
+                return false;
+            }
         }
     }
 }
